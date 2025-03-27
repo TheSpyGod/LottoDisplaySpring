46ff2014a1c417d069265190a53c57c11a7dab49
SpyGod <154527310+TheSpyGod@users.noreply.github.com>
2025-03-27 13:46:59 +0100

    Update WebSocketConfig.java

--- src/main/java/com/thespygod/lottobackend/WebSocketConfig.java
+++ src/main/java/com/thespygod/lottobackend/WebSocketConfig.java
@@ -18,7 +18,7 @@
         @Override
         public void configureMessageBroker(MessageBrokerRegistry registry) {
             registry.setApplicationDestinationPrefixes("/app");
-            registry.enableSimpleBroker("/topic");
+            registry.enableSimpleBroker("/broker");
         }
 
     }
